556b4464e76017e0a7149f36ec751fc9eded85ad
Arthur Mironov <arthur.mironov@progforce.com>
2019-09-21 09:08:04 +0300

    Init React Native

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Text>Hello world!</Text>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
